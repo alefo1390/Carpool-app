4f216425b1c9c909a6d9edd67f8c557bcf0ad78e
alefo1390 <ale7_90@live.it>
2026-03-11 23:06:56 +0100

    Update console logs in service worker events

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,25 @@
 self.addEventListener("install", event => {
-  console.log("App installata");
+
+console.log("Service Worker installato");
+
+});
+
+
+self.addEventListener("activate", event => {
+
+console.log("Service Worker attivo");
+
+});
+
+
+self.addEventListener("notificationclick", function(event){
+
+event.notification.close();
+
+event.waitUntil(
+
+clients.openWindow("./")
+
+);
+
 });
